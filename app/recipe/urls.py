c368e246ce78d095bb1a9156a273acca020a8d56
salman1s2h <salman1s2h@gmail.com>
2022-05-30 17:32:21 +0530

    tag part working fine

---
+++ app/recipe/urls.py
@@ -0,0 +1,22 @@
+"""
+URL mappings for the recipe app.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+router.register('tag',views.TagViewSet)
+
+app_name = 'recipe'#This is used when we want to used revses function
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
